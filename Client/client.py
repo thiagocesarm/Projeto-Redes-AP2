153a2b2cad2233a7bb42a3c4312a8bd0e13b67d9
Joao Emmanuel <joaoei@localhost.localdomain>
2018-05-12 20:06:52 -0300

    client socket added

---
+++ Client/client.py
@@ -0,0 +1,22 @@
+import socket
+
+HOST = '127.0.0.1'     # Server IP
+PORT = 12345           # Server port
+SIZE = 1024			   # Buffer sizer
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)
+s.connect(dest)
+
+print 'Connecting...\n'
+msg = raw_input('Write your message: ')
+
+if msg:
+    s.sendall(b'Message: {}'.format(msg))
+    print 'Send message\n'
+
+    response = s.recv(SIZE)
+    print 'Response: '+response;
+
+print 'Closing connection...'
+s.close()
