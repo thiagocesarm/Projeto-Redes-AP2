abd374abc1770b57712dc7e233bcb62f06cfc2a5
ireneginani <ireneginani@gmail.com>
2018-05-12 19:23:42 -0300

    modificado firewall adequação ao modo do proxy

--- Firewall/firewall.py
+++ Firewall/firewall.py
@@ -1,41 +1,86 @@
+# -*- coding: utf-8 -*-
 import socket
+from threading import Thread
 import sys
 
-class Socket:
-
+# Serve como um servidor para o proxy, mantém as conexões vindas dos clientes
+class ClientListener:
     def __init__(self):
-        self.sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ownIP = '127.0.0.1'
+        self.ownPort = 12345
+        self.serverIP = '127.0.0.1'
+        self.serverPort = 12346
+        self.size = 1024
+        self.server = None
+        self.connectedClients = []
 
-    def conexao(self, ip, port=1911):
-        self.sock.connect((ip, port))
+    def create_socket(self):
+        try:
+            print "Criando socket..."
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind((self.ownIP, self.ownPort))
+            self.server.listen(2)
+        except socket.error, (value, message):
+            print "Erro de socket: " + message
+            sys.exit(1)
 
-    def esperandoConexao(self, ip='', port=1911, l=1):
-        self.sock.bind((ip, port))
-        self.sock.listen(l) 
-        (clientsocket, address) = self.sock.accept()
-        self.sock = clientsocket 
-    
-    def enviar(self, msg):
-        sent = self.sock.send(msg)
-        if sent == 0:
-            raise BrokenCon, "socket connection broken"
+    def start_listening(self):
+        self.create_socket();
 
-    def receber(self, bytes):
-        return self.sock.recv(bytes)
+        connection, address = self.server.accept()
+        newConnection = ClientConnection(connection,
+                                            address,
+                                            self.serverIP,
+                                            self.serverPort,
+                                            self.size)
+        newConnection.start()
+        self.connectedClients.append(c)
 
-    def fechar(self):
-        self.sock.close()
+class ClientConnection(Thread):
+    def __init__(self, connection, address, serverIP, serverPort, size):
+        print "Cliente conectado: " + connection + " | endereço: " + address
+        Thread.__init__(self)
+        self.connection = connection
+        self.address = address
+        self.serverIP = serverIP
+        self.serverPort = serverPort
+        self.size = size
 
+    def run(self):
+        # Ips de exemplo que serão bloqueados pelo servidor por serem maliciosos
 
-   
-def enviaDecisao(skt, pwd):
+        ip_block = ['178.234.24.70','178.234.24.15','178.234.24.3']
+        
+        # Loop infinito até o cliente desconectar
 
+        # Recebe a mensagem do cliente
+        message = self.connection.recv(self.size)
 
-def recebendoRequisicao(skt, pwd):      
+        # Se recebeu
+        if message:
 
+            for ip in ip_block:
+                if (self.serverIP == ip):
+                    negado = True
+                   
+            if (negado):
+                msg = "Sua requisição foi negada"
+                self.connection.send(msg.encode())
 
-def Firewall():
-    
+            else:
+                # Repassa mensagem ao servidor (adicionar funcionalidade aqui)
+                serverSocket = socket.socket(socket.AF_INET, SOCK_STREAM)
+                serverSocket.connect((self.serverIP, self.serverPort))
+                serverSocket.sendall(message)
 
-if __name__ == '__main__':
-    Firewall()
+                # Recebe resposta do servidor e repassa ao usuario
+                response = serverSocket.recv(self.size)
+                self.connection.sendall(self.size)
+
+                # Se não recebeu cancela a conexão
+        else:
+            self.connection.close()
+
+if __name__ == "__main__":
+    s = ClientListener()
+    s.start_listening()
