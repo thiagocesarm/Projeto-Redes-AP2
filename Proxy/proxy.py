15057e85c171df314940cbb872bdd8a66d09f09c
Gustavo Carvalho <guga.a.carvalho@gmail.com>
2018-05-12 02:22:55 -0300

    Adicionado proxy básico

---
+++ Proxy/proxy.py
@@ -0,0 +1,74 @@
+import socket
+from threading import Thread
+import sys
+
+# Serve como um servidor para o proxy, mantém as conexões vindas dos clientes
+class ClientListener:
+	def __init__(self):
+		self.ownIP = '127.0.0.1'
+		self.ownPort = 12345
+		self.serverIP = '127.0.0.1'
+		self.serverPort = 12346
+		self.size = 1024
+		self.server = None
+		self.connectedClients = []
+
+	def create_socket(self):
+		try:
+			print "Criando socket..."
+			self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.server.bind((self.ownIP, self.ownPort))
+			self.server.listen()
+		except socket.error, (value, message):
+			print "Erro de socket: " + message
+			sys.exit(1)
+
+	def start_listening(self):
+		self.create_socket();
+
+        run = 1
+        while run:
+            connection, address = self.server.accept()
+            newConnection = ClientConnection(connection,
+            								 address,
+            								 self.serverIP,
+            								 self.serverPort,
+            								 self.size)
+            newConnection.start()
+            self.connectedClients.append(c)
+
+class ClientConnection(Thread):
+	def __init__(self, connection, address, serverIP, serverPort, size):
+		print "Cliente conectado: " + connection + " | endereço: " + address
+		Thread.__init__(self)
+		self.connection = connection
+		self.address = address
+		self.serverIP = serverIP
+		self.serverPort = serverPort
+		self.size = size
+
+	def run(self):
+		# Loop infinito até o cliente desconectar
+		while True:
+			# Recebe a mensagem do cliente
+			message = self.connection.recv(self.size)
+
+			# Se recebeu
+			if message:
+				# Repassa mensagem ao servidor (adicionar funcionalidade aqui)
+				serverSocket = socket.socket(socket.AF_INET, SOCK_STREAM)
+				serverSocket.connect((self.serverIP, self.serverPort))
+				serverSocket.sendall(message)
+
+				# Recebe resposta do servidor e repassa ao usuario
+				response = serverSocket.recv(self.size)
+				self.connection.sendall(self.size)
+
+			# Se não recebeu cancela a conexão
+			else:
+				self.connection.close()
+				break
+
+if __name__ == "__main__":
+	s = ClientListener()
+	s.start_listening()
